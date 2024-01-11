d434eb7e671dea4ff9fc8ee63385afc45e201d95
kingrtk1 <108303178+kingrtk1@users.noreply.github.com>
2024-01-12 00:51:11 +0600

    Update run.py

--- run.py
+++ run.py
@@ -4,12 +4,17 @@ def extract_blocks_with_keywords(file_path, search_terms, output_file_path):
     try:
         with open(file_path, 'r') as input_file, open(output_file_path, 'w') as output_file:
             current_block = []
+
             for line in input_file:
                 current_block.append(line)
 
                 if any(search_term.strip() == line.strip() for search_term in search_terms):
                     output_file.write(''.join(current_block))
                     current_block = []
+
+            # Check if there's an incomplete block at the end
+            if current_block:
+                output_file.write(''.join(current_block))
 
         print(f"search terms extracted and saved to: {output_file_path}")
     except FileNotFoundError:
@@ -20,19 +25,20 @@ def extract_blocks_with_keywords(file_path, search_terms, output_file_path):
 if __name__ == "__main__":
     # Add the logo
     logo = """
-SSSS   EEEEE  PPPP     A    RRRR     A    TTTTT  EEEEE  
-S      E      P   P   A A   R   R   A A     T    E      
- SSS   EEE    PPPP   AAAAA  RRRR   AAAAA    T    EEE    
-    S  E      P      A   A  R  R   A   A    T    E      
-SSSS   EEEEE  P      A   A  R   R  A   A    T    EEEEE  
-"""
+     SSS   EEEEE  PPPP     A    RRRR     A    TTTTT  EEEEE  
+    S      E      P   P   A A   R   R   A A     T    E      
+     SSS   EEE    PPPP   AAAAA  RRRR   AAAAA    T    EEE    
+        S  E      P      A   A  R  R   A   A    T    E      
+     SSS   EEEEE  P      A   A  R   R  A   A    T    EEEEE  
+    """
     print("\033[93m" + logo + "\033[0m")  # Use ANSI escape codes for color
 
     # Ask the user for input file, search terms, and manual output file path
     input_file_path = input("Enter the input file path: ")
+    
     search_terms = []
-
     print("Enter the search terms. Type 'done' on  new line when finished:")
+    
     while True:
         term = input()
         if term.lower() == 'done':
@@ -46,5 +52,6 @@ SSSS   EEEEE  P      A   A  R   R  A   A    T    EEEEE
 
     # Ask the user whether to continue
     response = input("Do you want to continue? (y/n): ")
+    
     if response.lower() != 'y':
         print("Exiting the script.")
